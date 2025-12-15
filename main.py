bf393e8db56954e1b20492aad6d84b4c0ead66d0
Lukash <lukashn159@gmail.com>
2025-12-15 19:19:59 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+print("Location of ISS :")
+print(f"Latitude = {iss_latitude} , Longitude = {iss_longitude}")
+
+
+
+
+
+
+
+
