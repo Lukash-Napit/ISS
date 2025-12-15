d2352f00a7995ab287f4eced258c88b359c56df9
Lukash <lukashn159@gmail.com>
2025-12-15 19:32:47 +0545

    Added distance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from geopy.distance import geodesic
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -8,6 +9,17 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 print("Location of ISS :")
 print(f"Latitude = {iss_latitude} , Longitude = {iss_longitude}")
+
+print("\n\n")
+
+print("Your latitude and longitude are :")
+lat,lon = float(input()),float(input())
+
+iss = (iss_latitude,iss_longitude)
+loc = (lat,lon)
+
+print(f"Iss is {geodesic(iss,loc).km} km far from your location .")
+
 
 
 
